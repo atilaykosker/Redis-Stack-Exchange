b3fded6addbf433ab0f3f7450236a8a696079b90
atovarkosker <41235268+atovarkosker@users.noreply.github.com>
2020-12-08 00:01:02 +0300

    Add project files.

---
+++ RedisExample/CacheService/Redis/RedisServer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExample.CacheService.Redis
+{
+    public class RedisServer
+    {
+        private ConnectionMultiplexer _connectionMultiplexer;
+        private IDatabase _database;
+        private string configurationString;
+        private int _currentDatabaseId = 0;
+
+        public RedisServer(IConfiguration configuration)
+        {
+            CreateRedisConfigurationString(configuration);
+
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(configurationString);
+            _database = _connectionMultiplexer.GetDatabase(_currentDatabaseId);
+        }
+
+        public IDatabase Database => _database;
+
+        public void FlushDatabase()
+        {
+            _connectionMultiplexer.GetServer(configurationString).FlushDatabase(_currentDatabaseId);
+        }
+
+        private void CreateRedisConfigurationString(IConfiguration configuration)
+        {
+            string host = configuration.GetSection("RedisConfiguration:Host").Value;
+            string port = configuration.GetSection("RedisConfiguration:Port").Value;
+
+            configurationString = $"{host}:{port}";
+        }
+    }
+}
---
+++ RedisExample/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedisExample.CacheService;
+using RedisExample.Models;
+
+namespace RedisExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> _books;
+        private ICacheService _cacheService;
+
+        public BooksController(ICacheService cacheService)
+        {
+            _cacheService = cacheService;
+
+            InitBookList();
+        }
+
+        public IActionResult Index()
+        {
+            if (_cacheService.Any("books"))
+            {
+                var books = _cacheService.Get<List<Book>>("books");
+                return Ok(books);
+            }
+
+            _cacheService.Add("books", _books);
+
+            return Ok(_books);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            _books.Add(book);
+            _cacheService.Remove("books");
+
+            return Ok("Books Added");
+        }
+
+        private void InitBookList()
+        {
+
+            if (_books == null)
+            {
+                _books = new List<Book>();
+                _books.Add(new Book(1, "Suç ve Ceza", "Dostoyevski"));
+                _books.Add(new Book(2, "Totem ve Tabu", "Freud"));
+                _books.Add(new Book(3, "Devrim Psikolojisi", "Le Bon"));
+                _books.Add(new Book(4, "Yaşam Bilgisi", "Adler"));
+            }
+        }
+    }
+}
